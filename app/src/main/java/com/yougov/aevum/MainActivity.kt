49b53e7712b10874b8f96464c8490f3212cc9ff2
Sutton1208 <ashley.suttondev@outlook.com>
2022-09-30 18:22:00 +0100

    Update MainActivity.kt

--- app/src/main/java/com/yougov/aevum/MainActivity.kt
+++ app/src/main/java/com/yougov/aevum/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -17,11 +18,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,6 +60,20 @@ fun TimersScreen(viewModel: MainViewModel,
                     onAddTimer: (String, String, String) -> Unit) {
     Surface {
         Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Please enter a timer.",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = TextStyle(fontSize = 24.sp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "The timer will appear in the list below",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = TextStyle(fontSize = 14.sp, color = Color.DarkGray)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
             NewTimer(onAddTimer)
             Spacer(modifier = Modifier.height(24.dp))
             Text("Running timers:")
@@ -96,7 +113,9 @@ fun NewTimer(onAddTimer: (String, String, String) -> Unit) {
             modifier = Modifier
                 .weight(1f)
                 .testTag("hours"),
-            onValueChange = { hours = it })
+            onValueChange = {
+                hours = it
+            })
 
         TimeInput(placeholderText = "minutes",
             modifier = Modifier
@@ -137,6 +156,7 @@ fun TimeInput(placeholderText: String, modifier: Modifier = Modifier, onValueCha
                 style = TextStyle(fontSize = 10.sp)
             )
         },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier
     )
 }
